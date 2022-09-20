1a40531bbb43b06a834999454833c386a8372f37
HrnTrkNl <demirisaev16@gmail.com>
2022-09-20 12:38:09 +0200

    App.js file conflict solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import "./App.css";
-import Home from "./page/Home";
 import logo from "./logo.svg";
+import "./App.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAllScores } from "./store/score/selectors";
