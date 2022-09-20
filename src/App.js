2ed4851a88a6dc7ab499070e9645cb15702ff47d
HrnTrkNl <demirisaev16@gmail.com>
2022-09-20 12:52:50 +0200

    add axios and redux packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAllScores } from "./store/score/selectors";
 import { getAllScores } from "./store/score/thunks";
+import Home from "./page/Home";
 
 function App() {
   const dispatch = useDispatch();
