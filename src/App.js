8b644a4103b90bab4af8001eaa127500f1627852
Atubak <mr-jona@hotmail.com>
2022-09-20 12:17:32 +0200

    add selectors and incomplete thunks for slices. getallscores should work when cors is allowed in router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {selectAllScores} from "./store/score/selectors";
+import { getAllScores } from './store/score/thunks';
 
 function App() {
+  const dispatch = useDispatch();
+  const allScores = useSelector(selectAllScores());
+  console.log(allScores);
+  useEffect(()=> {
+    dispatch(getAllScores());
+  },[])
   return (
     <div className="App">
       <header className="App-header">
