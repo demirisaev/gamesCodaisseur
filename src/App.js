a917d7afdede2a2325001828ecf36e222c1e4700
HrnTrkNl <demirisaev16@gmail.com>
2022-09-20 12:36:03 +0200

    Conflict files changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import Home from "./page/Home";
+import logo from "./logo.svg";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectAllScores } from "./store/score/selectors";
+import { getAllScores } from "./store/score/thunks";
 
 function App() {
   return (
