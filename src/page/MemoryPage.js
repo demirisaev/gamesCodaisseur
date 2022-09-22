ade1cbf1b0006041299d0bc26b76127861f472ea
Jona <mr-jona@hotmail.com>
2022-09-22 16:33:53 +0200

    add some imporvements

--- src/page/MemoryPage.js
+++ src/page/MemoryPage.js
@@ -217,13 +217,19 @@ export default function MemoryPage({ visibility, difficulty, diffClass }) {
       ) : (
         <>
           <iframe
-            width="560"
-            height="0"
+            width="100%"
+            height="100%"
             src="https://www.youtube.com/embed/6xKWiCMKKJg?&autoplay=1"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
+            style={{
+              position: "absolute",
+              top: "0px",
+              left: "0px",
+              transform: "translate(-50%, -50%, -50%, -50%)",
+            }}
           ></iframe>
           <div
             className={` ${pokevis ? "balls" : "pokevis"}`}
